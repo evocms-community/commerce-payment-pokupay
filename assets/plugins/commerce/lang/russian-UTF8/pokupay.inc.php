576ef0dbf80af533da1831cde665896effb8c0da
mnoskov <nm@perm-tools.ru>
2019-09-19 09:59:19 +0500

    title changed

--- assets/plugins/commerce/lang/russian-UTF8/pokupay.inc.php
+++ assets/plugins/commerce/lang/russian-UTF8/pokupay.inc.php
@@ -3,7 +3,7 @@
 setlocale(LC_ALL, 'ru_RU.UTF-8');
 
 return [
-    'pokupay.caption' => 'Оплата в кредит через Сбербанк Онлайн',
+    'pokupay.caption' => 'В кредит от "Покупай со Сбербанком"',
     'pokupay.error_empty_token_and_login_password' => 'Укажите токен или логин/пароль в настройках плагина!',
     'pokupay.link_caption' => 'Ссылка на оплату',
     'pokupay.letter_link' => '@CODE:<p>Для подачи заявки на кредит вы можете перейти по ссылке: <a href="[+link+]" target="_blank">[+link+]</a></p>',
